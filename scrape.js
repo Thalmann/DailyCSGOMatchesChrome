89a206f64632cf4dd4973aa45314f2071663283b
Thalmann <mr.thalmann@gmail.com>
2015-10-29 18:10:34 +0100

    Waiting for events and updating popup on event

---
+++ scrape.js
@@ -0,0 +1,10 @@
+var url = "http://hltv.org/results";
+//var r = xhhtp.open("GET", url, false);
+
+function getContent(){
+    document.write("WTF");
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  document.querySelector('button').addEventListener('click', getContent);
+});
