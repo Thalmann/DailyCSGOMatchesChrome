a76d708ed438c041e01d1738ad72255d0be9f9b5
Thalmann <mr.thalmann@gmail.com>
2015-10-30 22:57:13 +0100

    Added event to wait for when the request is executed

--- scrape.js
+++ scrape.js
@@ -1,9 +1,21 @@
-var url = "http://hltv.org/results";
-var xhttp = new XMLHttpRequest();
-var r = xhttp.open("GET", url, true);
+
 
 function getContent(){
-    document.write(r.getResponseHeader());
+    var url = "http://hltv.org/results/";
+    var xhttp = new XMLHttpRequest();
+
+    xhttp.onreadystatechange=function()
+    {
+        if (xhttp.readyState==4 && xhttp.status==200)
+        {
+            console.log(xhttp.responseText);
+	    document.write(xhttp.responseText);
+	    console.log("HEJ");
+        }
+    }
+    
+    xhttp.open("GET", url, true);
+    xhttp.send();
 }
 
 document.addEventListener('DOMContentLoaded', function () {
